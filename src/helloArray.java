24088b0e51cab00caac8ac39e2b49040a562d7d8
PREDATOR <prateek772492@gmail.com>
2022-07-12 10:32:56 +0530

    2

--- src/helloArray.java
+++ src/helloArray.java
@@ -1,3 +1,5 @@
+import java.security.spec.RSAOtherPrimeInfo;
+
 public class helloArray {
     public static void main(String[] args) {
         int arr[] = {10,20,30,40,50,60};
@@ -14,8 +16,15 @@ public class helloArray {
 
         int arr1[][] = {{10,20,30}, {40,50,60}, {70,80,90}};
 
-        for(int i = 0; i<arr1.length; i++){
-            System.out.println(arr1.[i]);
+//        for(int i = 0; i<arr1.length; i++){
+//            System.out.println(arr1[i]);
+//        }
+
+        for(int i = 0; i<3; i++){
+            for(int j = 0; j<3; j++){
+                System.out.println(arr1[i][j] + " , ");
+            }
+            System.out.println();
         }
     }
 }
