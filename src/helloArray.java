79e46972f35d30ed7418aef8bdfbda9a5b4464c5
PREDATOR <prateek772492@gmail.com>
2022-07-12 10:15:43 +0530

    1

--- src/helloArray.java
+++ src/helloArray.java
@@ -7,5 +7,15 @@ public class helloArray {
         for(int index = 0; index<arr.length; index++) {
             System.out.println(arr[index]);
         }
+
+        for(int value : arr){
+            System.out.println(value);
+        }
+
+        int arr1[][] = {{10,20,30}, {40,50,60}, {70,80,90}};
+
+        for(int i = 0; i<arr1.length; i++){
+            System.out.println(arr1.[i]);
+        }
     }
 }
